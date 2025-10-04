013ec2c0ea857817d71245593851dc584a5b8250
Imanol Sanchez <113470311+dernest0@users.noreply.github.com>
2025-10-04 02:32:03 +0000

    emoji

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ form.addEventListener("submit", function (event) {
   teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
 
   // Show welcome Message
-  const message  = `welcome, ${name} from ${teamName}`;
+  const message  = `🎉 welcome, ${name} from ${teamName}`;
   console.log(message);
 
   form.reset();
